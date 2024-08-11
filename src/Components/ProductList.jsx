c64fa46c18593faa752c8d427b0766991bfaea7f
Ardhika Putra <ardhikaptr11@gmail.com>
2024-08-11 17:43:48 +0700

    Updating Code

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,22 +1,54 @@
-import React from 'react';
-import './ProductList.css'; 
+// ProductList.jsx
+import React, { useState, useEffect } from "react";
+import "./ProductList.css";
+import { useDispatch, useSelector } from "react-redux";
+import { addItemToCart, removeItemFromCart } from "./CartSlice";
+import ShoppingCart from "./ShoppingCart";
+import { cartItems } from "./ShoppingCart";
 
 const ProductList = () => {
+	const products = [
+		{ id: 1, name: "T-shirt", price: 60 },
+		{ id: 2, name: "Blazer", price: 75 },
+		{ id: 3, name: "Short Pants", price: 30 }
+	];
 
-  const products = [
-    { id: 1, name: 'Product A', price: 60 },
-    { id: 2, name: 'Product B', price: 75 },
-    { id: 3, name: 'Product C', price: 30 },
-  ];
+	const dispatch = useDispatch();
+	const cartItems = useSelector((state) => state.cart.cartItems);
+	const [isCartVisible, setIsCartVisible] = useState(false);
 
-  return (
-    <div className="product-list">
-      <h2 className="product-list-title">Products</h2>
-      <ul className="product-list-items">
-     
-      </ul>
-    </div>
-  );
+	useEffect(() => {
+		if (cartItems.length === 0) {
+			setIsCartVisible(false);
+		} else {
+			setIsCartVisible(true);
+		}
+	}, [cartItems]);
+
+	const handleAddToCart = (product) => {
+		dispatch(addItemToCart(product));
+	};
+
+	const listOfProduct = products.map((product) => {
+		return (
+			<li key={product.id} className="product-list-item">
+				<span>
+					{product.name} - ${product.price}
+				</span>
+				<button className="add-to-cart-btn" onClick={() => handleAddToCart(product)}>
+					Add to Cart
+				</button>
+			</li>
+		);
+	});
+
+	return (
+		<div className="product-list">
+			<h2 className="product-list-title">Products</h2>
+			<ul className="product-list-items">{listOfProduct}</ul>
+			{isCartVisible && <ShoppingCart />}
+		</div>
+	);
 };
 
 export default ProductList;
--- src/Components/ShoppingCart.jsx
+++ src/Components/ShoppingCart.jsx
@@ -1,20 +1,71 @@
-import React from 'react';
-import './ShoppingCart.css'; 
+import React from "react";
+import "./ShoppingCart.css";
+import { useDispatch, useSelector } from "react-redux";
+import {
+	removeItemFromCart,
+	clearCart,
+	increaseItemQuantity,
+	decreaseItemQuantity
+} from "./CartSlice.jsx";
+import "./ShoppingCart.css";
 
 const ShoppingCart = () => {
+	const dispatch = useDispatch();
+	const cartItems = useSelector((state) => state.cart.cartItems);
+	const totalAmount = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
 
-  return (
-    <>
-    <div className="shopping-cart">
-      <h2 className="shopping-cart-title">Shopping Cart</h2>
-      <ul className="cart-items">
-       
-      </ul>
-      <button className="clear-cart-btn">Clear Cart</button>
-    </div>
-  
-    </>
-  );
+	const handleRemoveItem = (itemId) => {
+		dispatch(removeItemFromCart(itemId));
+	};
+
+	const handleClearCart = () => {
+		dispatch(clearCart());
+	};
+
+	const handleIncreaseQuantity = (itemId) => {
+		dispatch(increaseItemQuantity(itemId));
+	};
+
+	const handleDecreaseQuantity = (itemId) => {
+		dispatch(decreaseItemQuantity(itemId));
+	};
+
+	const productCartItem = cartItems.map((item) => (
+		<li key={item.id} className="cart-item">
+			<span>
+				{item.name} - ${item.price}
+			</span>
+			<div className="quantity-controls">
+				<button
+					className="quantity-control-btn decrease"
+					onClick={() => handleDecreaseQuantity(item.id)}>
+					-
+				</button>
+				<span className="item-qty"> {item.quantity}</span>
+				<button
+					className="quantity-control-btn increase"
+					onClick={() => handleIncreaseQuantity(item.id)}>
+					+
+				</button>
+			</div>
+			<button className="remove-item-btn" onClick={() => handleRemoveItem(item.id)}>
+				Remove
+			</button>
+		</li>
+	));
+
+	return (
+		<>
+			<div className="shopping-cart">
+				<h2 className="shopping-cart-title">Shopping Cart</h2>
+				<ul className="cart-items">{productCartItem}</ul>
+				<div>{totalAmount ? <div>The total amount is ${totalAmount}</div> : ""}</div>
+				<button className="clear-cart-btn" onClick={handleClearCart}>
+					Clear Cart
+				</button>
+			</div>
+		</>
+	);
 };
 
 export default ShoppingCart;
